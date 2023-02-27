f5fc3fb69e7d81af1b1f10a99963e75fc7976487
AbdurRehman801 <bhayrehman1@gmail.com>
2023-02-27 19:21:42 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Navbar from './Components/Navbar';
 import {auth} from './firebase'
 import {useAuthState} from 'react-firebase-hooks/auth'
