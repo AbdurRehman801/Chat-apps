3ca435909535a025ef3e6ccf20e7b3b3a01633dd
AbdurRehman801 <bhayrehman1@gmail.com>
2023-02-28 13:14:55 +0500

    adding signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import {auth} from './firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
-import Chat from './Components/Chat';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Chat from "./Components/Chat";
+import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
 };
 function App() {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
+  getRedirectResult(auth)
+    .then((result) => {
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+
+
+
+    });
   return (
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
-      <Navbar/>
-      <Chat/>
+        <Navbar />
+        {user ? <Chat /> : null}
       </section>
     </div>
   );
 }
-// {user ? <Chat/> : null}
 export default App;
